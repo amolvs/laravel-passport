c2309a46e4d9432325070df4c1aedf7fb8819b4c
amol <amolvsakhare@gmail.com>
2020-07-28 19:26:21 +0530

    basic laravel(5.8) code with passport

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+	public function register(Request $request)
+	{
+		$validatedData = $request->validate([
+			'name'=>'required|max:55',
+			'email'=>'email|required|unique:users',
+			'password'=>'required|confirmed'
+		]);
+
+		$validatedData['password'] = bcrypt($request->password);
+
+		$user = User::create($validatedData);
+
+		$accessToken = $user->createToken('authToken')->accessToken;
+
+		return response(['user'=> $user, 'access_token'=> $accessToken]);
+	}
+
+
+	public function login(Request $request)
+	{
+		$loginData = $request->validate([
+			'email' => 'email|required',
+			'password' => 'required'
+		]);
+
+		if(!auth()->attempt($loginData)) {
+			return response(['message'=>'Invalid credentials']);
+		}
+
+		$accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+		return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+	}
+
+
+	public function logout(Request $request)
+	{
+		$isUser = auth()->user()->token()->revoke();
+		if ($isUser) {
+			return response(['message' => 'Successfully logged out.']);
+		} else {
+			return response(['message' => 'Something went wrong.']);
+		}
+	}
+
+
+	public function getUser()
+	{
+		return response(['user' => auth()->user()]);
+	}
+}
